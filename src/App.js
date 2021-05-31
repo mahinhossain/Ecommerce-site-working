2ee62f47c5a7ab97dfccfba9dda5abe3f7f49c09
Md. Mahin Hossain <intern67.it@akij.net>
2021-05-31 17:47:56 +0600

    kk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Products from "./components/Products";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+
+      <Switch>
+        <Route exact path="/"></Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/products">
+          <Products />
+        </Route>
+
+        <Route path="/contacts">
+          <Contacts />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
